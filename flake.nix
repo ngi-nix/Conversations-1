509563239526ed8b7f0bbeac1c56a426feb6ff55
Mazurel <mateusz.mazur@yahoo.com>
2021-09-27 11:31:56 +0200

    Polishing up Nix build system

--- flake.nix
+++ flake.nix
@@ -1,3 +1,12 @@
+# This file allows building Conversations with Nix build system.
+# To learn more about nix, see: https://nixos.org/
+# To enable flakes (required for building this project), see:
+# https://nixos.wiki/wiki/Flakes
+# To update dependencies (if this build fails), please run:
+# > nix develop .
+# > generate --root-dir ../. --jobs <Num of your cores>
+# > exit
+# Profit ! (aka it should build)
 {
   description = "Conversations is an open source XMPP/Jabber client for Android ";
 
@@ -5,40 +14,41 @@
 
   outputs = { self, android2nix }:
     android2nix.lib.mkAndroid2nixEnv (
-      { stdenv, callPackage, gradle, jdk11, ... }: {
-      pname = "Conversations";
-
-      src =
-        let
-          # Currently webrtc is fetched form maven repo,
-          # it should be compiled manually in the future.
-          # It is really complicated right now with Nix (due to gsync).
-          webrtc = callPackage ./nix/webrtc.nix { };
-        in stdenv.mkDerivation {
+      { stdenv, callPackage, gradle, jdk11, ... }: rec {
         pname = "Conversations";
-        version = "dev";
-        src = ./.;
-        dontBuild = true;
-        dontConfigure = true;
+        src =
+          let
+            # Currently webrtc is fetched form maven repo,
+            # it should be compiled manually in the future.
+            # It is really complicated right now with Nix (due to gsync).
+            webrtc = callPackage ./nix/webrtc.nix {};
+          in
+            stdenv.mkDerivation {
+              inherit pname;
+              version = "dev";
+              src = ./.;
+              dontBuild = true;
+              dontConfigure = true;
 
-        installPhase = ''
-          mkdir -p $out
-          cp -rf ./* $out/
-        '';
+              installPhase = ''
+                mkdir -p $out
+                cp -rf ./* $out/
+              '';
 
-        fixupPhase = ''
-          cd $out
+              fixupPhase = ''
+                cd $out
 
-          mkdir libs
-          cp ${webrtc} libs/libwebrtc-m92.aar
-          cp ${webrtc} libs/libwebrtc.aar
-        '';
-      };
+                mkdir libs
+                cp ${webrtc} libs/libwebrtc-m92.aar
+                cp ${webrtc} libs/libwebrtc.aar
+              '';
+            };
 
-      gradlePkg = gradle;
-      jdk = jdk11;
-      devshell = ./nix/devshell.toml;
-      deps = ./nix/deps.json;
-      buildType = "assembleConversationsFreeSystemDebug";
-    });
+        gradlePkg = gradle;
+        jdk = jdk11;
+        devshell = ./nix/devshell.toml;
+        deps = ./nix/deps.json;
+        buildType = "assembleConversationsFreeSystemDebug";
+      }
+    );
 }
